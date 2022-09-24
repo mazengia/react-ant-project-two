b74bb7403723038f8006b06a4fe9644cb14e85fe
mtesfa <mz.tesfa@gmail.com>
2022-09-24 08:46:36 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,151 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+    AppstoreOutlined,
+    DashboardOutlined,
+    HomeOutlined, MenuUnfoldOutlined, ProfileOutlined,
+    UserOutlined,
+} from '@ant-design/icons';
+import 'antd/dist/antd.min.css';
+import { Menu} from 'antd';
+import React, {useState} from 'react';
+import {Route, Routes, useNavigate} from "react-router-dom";
+import Homepage from "./components/Home"
+function getItem(label, key, icon, children, type) {
+    return {
+        key,
+        icon,
+        children,
+        label,
+        type,
+    };
+}
+const ListOfItems = [
+    getItem('Home', 'Home', <HomeOutlined/>),//PieChartOutlined is icon
+    getItem('Dashboard', 'Dashboard', <DashboardOutlined/>),
+    getItem('Users', 'Users', <UserOutlined/>),
+    getItem('Profile', 'Profile', <ProfileOutlined/>),
+    getItem('Navigation Two', 'sub2', <AppstoreOutlined/>, [
+        getItem('Option 9', '9'),
+        getItem('Option 10', '10'),
+        getItem('Submenu', 'sub3', null, [getItem('Option 11', '11'), getItem('Option 12', '12')]),
+    ]),
+    getItem('', 'Collapse', <MenuUnfoldOutlined/>),
+];
+const App = () => {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    return (
+        <div
+            style={{
+                display: "flex",
+                flexDirection: "column",
+                flex: 1,
+                height: '100vh'
+            }}>
+            <Header/>
+            <div
+                style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    flex: 1,
+                }}>
+                <SideMenu/>
+                <Content/>
+            </div>
+            <Footer/>
+        </div>
+    );
+};
+
+function SideMenu() {
+
+    let [collapsed, setCollapsed,width] = useState(false);
+
+    const toggleCollapsed = () => {
+        setCollapsed(!collapsed);
+        if(!collapsed){
+              width = 56;
+        }
+        else {
+              width = 200 ;
+        }
+    };
+    const navigate = useNavigate();
+    return (
+        <div
+            style={{
+                width: width,
+                display: "flex",
+                flexDirection: "column",
+            }}
         >
-          Learn React
-        </a>
-      </header>
+            <Menu
+                style={{
+
+                    height: '100%'
+                }}
+                onClick={({key}) => {
+                    if (key === "Collapse") {
+                        toggleCollapsed(key);
+                    } else {
+                        navigate(key)
+                    }
+                }}
+                defaultSelectedKeys={['1']}
+                theme="dark"
+                inlineCollapsed={collapsed}
+                items={ListOfItems}
+            />
+        </div>);
+}
+
+function Content() {
+    return <div
+        style={{
+            width: '100%',
+        }}
+    >
+        <Routes>
+            <Route path="/" element={<div>empty </div>}></Route>
+            <Route path="/Home" element={<Homepage/> }></Route>
+            <Route path="/Dashboard" element={<div>Dashboard</div>}></Route>
+            <Route path="/Users" element={<div>Users</div>}></Route>
+            <Route path="/Profile" element={<div>Profile</div>}></Route>
+            <Route path="/9" element={<div>option 9</div>}></Route>
+            <Route path="/10" element={<div>option 10</div>}></Route>
+        </Routes>
     </div>
-  );
+}
+
+function Header() {
+    return (
+        <div
+            style={{
+                textAlign: "center",
+                justifyContent: "center",
+                alignItems: "center",
+                color: "white",
+                height: 60,
+                backgroundColor: "blue",
+                display: "flex",
+                fontWeight: "bold",
+            }}>
+            Header
+        </div>
+    );
+}
+
+function Footer() {
+    return (
+        <div style={{
+            textAlign: "center",
+            color: "white",
+            height: 30,
+            backgroundColor: "black",
+            alignItems: "center",
+            display: "flex",
+            fontWeight: "bold",
+            justifyContent: "center"
+        }}>mz.tesfa sc</div>
+    );
 }
 
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,163 @@
+import {Button, Drawer, Form, Input, Table} from 'antd';
+import React, {useState} from 'react';
+
+const Login = () => {
+    const onFinish = (values) => {
+        console.log('Success:', values);
+    };
+
+    const onFinishFailed = (errorInfo) => {
+        console.log('Failed:', errorInfo);
+    };
+    const [open, setOpen] = useState(false);
+
+    const showDrawer = () => {
+        setOpen(true);
+    };
+
+    const onClose = () => {
+        setOpen(false);
+    };
+    const columns = [
+        {
+            title: 'Name',
+            dataIndex: 'name',
+        },
+        {
+            title: 'Chinese Score',
+            dataIndex: 'chinese',
+            sorter: {
+                compare: (a, b) => a.chinese - b.chinese,
+                multiple: 3,
+            },
+        },
+        {
+            title: 'Math Score',
+            dataIndex: 'math',
+            sorter: {
+                compare: (a, b) => a.math - b.math,
+                multiple: 2,
+            },
+        },
+        {
+            title: 'English Score',
+            dataIndex: 'english',
+            sorter: {
+                compare: (a, b) => a.english - b.english,
+                multiple: 1,
+            },
+        },
+    ];
+    const data = [
+        {
+            key: '1',
+            name: 'John Brown',
+            chinese: 98,
+            math: 60,
+            english: 70,
+        },
+        {
+            key: '2',
+            name: 'Jim Green',
+            chinese: 98,
+            math: 66,
+            english: 89,
+        },
+        {
+            key: '3',
+            name: 'Joe Black',
+            chinese: 98,
+            math: 90,
+            english: 70,
+        },
+        {
+            key: '4',
+            name: 'Jim Red',
+            chinese: 88,
+            math: 99,
+            english: 89,
+        },
+    ];
+
+    const onChange = (pagination, filters, sorter, extra) => {
+        console.log('params', pagination, filters, sorter, extra);
+    };
+
+    return (
+        <>
+            <div style={{marginLeft:987}}>
+                <Button   onClick={showDrawer}>
+                    Add New Item
+                </Button>
+            </div>
+            <Table columns={columns} dataSource={data} onChange={onChange} />
+            <Drawer title="Basic Drawer" placement="right" onClose={onClose} open={open}>
+                <Form
+                    style={{marginTop:100}}
+                    name="basic"
+                    labelCol={{
+                        span: 8,
+                    }}
+                    wrapperCol={{
+                        span: 16,
+                    }}
+                    initialValues={{
+                        remember: true,
+                    }}
+                    onFinish={onFinish}
+                    onFinishFailed={onFinishFailed}
+                    autoComplete="off"
+                >
+                    <Form.Item
+                        label="Username"
+                        name="username"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your username!',
+                            },
+                        ]}
+                    >
+                        <Input />
+                    </Form.Item>
+
+                    <Form.Item
+                        label="Password"
+                        name="password"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your password!',
+                            },
+                        ]}
+                    >
+                        <Input.Password />
+                    </Form.Item>
+
+                    <Form.Item
+                        name="remember"
+                        valuePropName="checked"
+                        wrapperCol={{
+                            offset: 8,
+                            span: 16,
+                        }}
+                    >
+                    </Form.Item>
+                    <Form.Item
+                        wrapperCol={{
+                            offset: 8,
+                            span: 16,
+                        }}
+                    >
+                        <Button type="primary" htmlType="submit">
+                            Submit
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </Drawer>
+        </>
+
+    );
+};
+
+export default Login;
